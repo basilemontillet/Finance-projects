d898857cc8be1e5371783d7ac32c67046244680d
basilemontillet <basile.montillet@edhec.com>
2025-03-29 14:20:21 +0000

    Added SABR model

--- sabr_model.py
+++ sabr_model.py
@@ -1,2 +1,16 @@
 import numpy as np
 import yfinance
+
+"""
+Stochastic Alpha Beta Rho model
+
+Alpha = volatility level
+Beta = forward elasticity
+Rho = correlation between asset price and volatility
+"""
+
+
+
+
+
+
